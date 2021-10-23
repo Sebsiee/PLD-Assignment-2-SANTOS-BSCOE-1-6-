669aa0050ce8a3615fcf281ff659ca722da51be5
Sebastian <santosbasty4@gmail.com>
2021-10-23 20:01:02 +0800

    Second Commit

--- Name, Age, & Address.py
+++ Name, Age, & Address.py
@@ -2,4 +2,5 @@ print("A pleasant day!")
 name=str(input("Input name:"))
 age=int(input("Input age:"))
 address=str(input("Input address:"))
-print
+print("Hi, my name is " + str(name) + ". I am " + str(age) + " years old and I live in " + str(address) + ".")
+print("Good bye. Until we meet again " + str(name) + "!")
