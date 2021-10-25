74f1ae24ae78596bacd6583893af83706d6add07
Sebastian <santosbasty4@gmail.com>
2021-10-25 21:18:17 +0800

    Second Commit

--- Money & Apple.py
+++ Money & Apple.py
@@ -3,10 +3,9 @@ moneyAmount=int(input("Enter the amount of your money: "))
 applePrice=int(input("What is the price of an apple? "))
 totalApples=(moneyAmount / applePrice)
 totalChange=(moneyAmount - applePrice )
-appleOrNot = totalApples
-if appleOrNot > 1:
- print("You can buy " + str("%.0f" % appleOrNot) + " apples and your change is " + str(totalChange))  
-elif appleOrNot == 1:
- print("You can buy " + str("%.0f" % appleOrNot) + " apple and your change is " + str(totalChange))
+if totalApples > 1:
+ print("You can buy " + str("%.0f" % totalApples) + " apples and your change is " + str(totalChange) + " pesos.")  
+elif totalApples == 1:
+ print("You can buy " + str("%.0f" % totalApples) + " apple and your change is " + str(totalChange) + " pesos.")
 else:
- print("You can't buy an apple and your change is " + str(totalChange))
+ print("You can't buy an apple and your change is " + str(totalChange) + " pesos.")
