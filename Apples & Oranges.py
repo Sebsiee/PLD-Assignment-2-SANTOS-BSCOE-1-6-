bff4627624af880aece41e48fbaa22054427d1aa
Sebastian <santosbasty4@gmail.com>
2021-10-25 19:48:11 +0800

    First Commit

---
+++ Apples & Oranges.py
@@ -0,0 +1,11 @@
+print("Welcome to the Fruit Market!")
+print("The prices of the frutis are:")
+print("Apple=20, Orange=25")
+priceApples = 20
+priceOranges = 25
+apples=int(input("How many apples do you want to buy?"))
+oranges=int(input("How many oranges do you want to buy?"))
+total=(apples * priceApples + oranges * priceOranges)
+print("Total amount is:" + str(total))
+print("Solution:")
+print((str(apples)) + " * " + (str(priceApples)) + " + " + (str(oranges)) + " * " + (str(priceOranges)) + " = " + (str(total)))
